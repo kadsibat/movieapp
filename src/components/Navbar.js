348860da52fbda584aa5990a26b7d03bba40ba64
Batuhan <yesiltas_sibel@hotmail.com>
2022-04-04 13:55:41 +0300

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-
+import React, { useContext } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import { AuthContext } from "../context/AuthContext";
+import { signOut } from "firebase/auth";
+import { auth } from "../auth/firebase-config";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
+  const navigate =useNavigate()
+  const logOut = async () =>{
+      await signOut(auth)
+      navigate("/")
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "0 2rem",
+          }}
+        >
           <IconButton
             size="large"
             edge="start"
@@ -20,16 +34,25 @@ const Navbar = () => {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            Movie App
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Box>
+          {currentUser ? (
+              <Button color="inherit">user</Button>
+            ) : (
+              <Button onClick={() => navigate("/login")} color="inherit">login</Button>
+            )}
+            {currentUser ? (
+              <Button onClick={() => logOut} color="inherit">logout</Button>
+            ) : (
+              <Button onClick={() => navigate("/register")} color="inherit">register</Button>
+            )}
+
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
